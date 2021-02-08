fda70f52c7d9dee83f2b83d1a9b9895243b87b54
Ivan Perez <ivan.perezdominguez@nasa.gov>
2021-02-07 21:46:59 -0500

    Streamize.

--- copilot/examples/SNL.hs
+++ copilot/examples/SNL.hs
@@ -12,43 +12,96 @@
 -- It is based on a stragegy of 1) introducing new stream combinators for the
 -- keywords in the language, 2) adding CPP define entries to prevent conflicts
 -- with the host language, and remove keywords that don't have meaning.
+--
+-- As an example, we follow the structure of:
+-- Malvin et al. "EARS (Easy Approach to Requirements Syntax)", 2009.
 
-import Control.Monad (filterM)
+import Copilot.Core.Type
+import Copilot.Language
+import Prelude           hiding ((/=), (<), (>))
 
 -- Language keywords
 #define if if_
 #define when when_
+#define while while_
+#define during while_
+#define where where_
+#define is &
 #define shall
 #define satisfy
 #define the
-#define is &
+#define then
 
 -- Components
 #define system
 
-type Stream a = a
-
--- External variables
-
-moving    = True
-auto      = 0
-engaged 0 = True
-engaged _ = False
-speed     = 10
-
--- Tempoeral operators
-(==>) :: Stream Bool -> Stream Bool -> Stream Bool
-(==>) False _ = True
-(==>) _     x = x
-
+-- * Language keywords
 when_ :: Stream Bool -> Stream Bool -> Stream Bool
 when_ = (==>)
+  -- See also edge to treat 'when' as a trigger
+  -- where
+  --   -- True when a stream goes from false to true.
+  --   edge stream = stream && not previousStream
+  --     where
+  --       previousStream = [False] ++ stream
 
 if_ :: Stream Bool -> Stream Bool -> Stream Bool
 if_ = (==>)
 
+while_ :: Stream Bool -> Stream Bool -> Stream Bool
+while_ = (==>)
+
+where_ :: Stream Bool -> Stream Bool -> Stream Bool
+where_ = (==>)
+
 (&) = flip ($)
 
--- Examples
-req1 =
+-- * Examples
+
+-- ** Ubiquotous requirements
+
+req1 :: Stream Bool
+req1 = the system shall satisfy (speed < 10)
+
+-- ** Unwanted behaviors
+
+req2 :: Stream Bool
+req2 = if (auto is engaged) then (the system shall satisfy (speed < 10))
+
+-- ** State-driven requirements
+
+req3 :: Stream Bool
+req3 = while (auto is engaged) (the system shall satisfy (speed < 10))
+
+req4 :: Stream Bool
+req4 = during takeoff (the system shall satisfy (speed < 10))
+
+--
+req5 :: Stream Bool
+req5 =
   when moving (if (auto is engaged) (the system shall satisfy (speed < 10)))
+
+-- ** Optional features
+req6 :: Stream Bool
+req6 =
+  where gpsAvailable (the system shall satisfy (speed < 10))
+
+-- * External variables
+
+moving :: Stream Bool
+moving = true -- define as extern "moving" Nothing
+
+gpsAvailable :: Stream Bool
+gpsAvailable = true -- define as extern "gpsAvailable" Nothing
+
+takeoff :: Stream Bool
+takeoff = true -- define as extern "takeoff" Nothing
+
+auto :: Stream Int64
+auto = 0
+
+engaged :: Stream Int64 -> Stream Bool
+engaged x = x /= 0
+
+speed :: Stream Int64
+speed = 10
