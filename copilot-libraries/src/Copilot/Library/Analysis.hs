b88429a2b040271cfbe7039fef7c2f426ce08645
Ivan Perez <ivan.perezdominguez@nasa.gov>
2025-09-01 16:15:19 +0000

    Unimplementable.

--- copilot-libraries/src/Copilot/Library/Analysis.hs
+++ copilot-libraries/src/Copilot/Library/Analysis.hs
@@ -21,6 +21,13 @@ triviallyFalse :: Stream Bool -> Prop Universal
 triviallyFalse = triviallyTrue . not
 
 -- | Property that captures whether a list of streams is incompatible, meaning
--- that there is no way to make all streams true at the same time.
+-- that there is no way to make all streams true at the same time, ever.
 incompatible :: [Stream Bool] -> Prop Universal
 incompatible = forAll . not . foldr (&&) true
+
+-- | Property that captures whether a list of requirements leads to an
+-- unimplementable system, meaning that there is no way to make a system that
+-- will satisfy all properties at all times. Any realization of the system will
+-- invariably lead to requirements being violated at some point.
+unimplementable :: [Stream Bool] -> Prop Universal
+unimplementable = forAll . not . eventuallyPrev . foldr (&&) true
