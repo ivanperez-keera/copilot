df5fa679ae94134c049720933ea9aa618cca8ae1
Ivan Perez <ivan.perezdominguez@nasa.gov>
2025-11-11 19:18:56 +0000

    Documentation

--- copilot/examples/SNLAlt.hs
+++ copilot/examples/SNLAlt.hs
@@ -1,3 +1,17 @@
+-- | This example demonstrates how it is possible to add new keywords in
+-- Copilot, and to create a language that resembles plain language (e.g.,
+-- English).
+--
+-- The goal of this example is two-fold: first, we want to demonstrate that
+-- users are not limited by the Copilot language, that it is extensible, and
+-- that they can adjust it to fit their needs, and second, we want to
+-- demonstrate that users could make properties resemble a language that is
+-- most convenient for them.
+--
+-- As an example, we follow the structure of:
+-- Malvin et al. "EARS (Easy Approach to Requirements Syntax)", 2009.
+--
+-- For an alternative representation, see the file `SNL.hs`.
 import Copilot.Core.Type
 import Copilot.Language
 import Prelude          hiding ((>))
